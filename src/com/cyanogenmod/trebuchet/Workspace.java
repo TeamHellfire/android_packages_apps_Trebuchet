787d3b960b82ab3d02d095f622e608f9a3a65a94
nebkat <nebkat@teamhacksung.org>
2013-01-06 15:56:16 +0000

    Workspace: Only show hotseat outlines if hovering over
    Change-Id: Ib5b3373cec6d54f6136fca6a1a6663c1fe93ef9b

--- src/com/cyanogenmod/trebuchet/Workspace.java
+++ src/com/cyanogenmod/trebuchet/Workspace.java
@@ -505,7 +505,6 @@ public class Workspace extends PagedView
         updateChildrenLayersEnabled(false);
         mLauncher.lockScreenOrientation();
         setChildrenBackgroundAlphaMultipliers(1f);
-        mLauncher.getHotseat().setChildrenOutlineAlpha(1f);
         // Prevent any Un/InstallShortcutReceivers from updating the db while we are dragging
         InstallShortcutReceiver.enableInstallQueue();
         UninstallShortcutReceiver.enableUninstallQueue();
@@ -3416,6 +3415,8 @@ public class Workspace extends PagedView
                 }
             }
         }
+
+        mLauncher.getHotseat().setChildrenOutlineAlpha(mLauncher.isHotseatLayout(layout) ? 1f : 0f);
 
         // Handle the drag over
         if (mDragTargetLayout != null) {
